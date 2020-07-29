2d33d78820591650aa1e805bcfa4922b9213c778
Pablo Menz <p.menz.r@gmail.com>
2020-07-29 06:40:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,12 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  
   attr_reader :brand
-
+  
+  BRANDS=[]
+  
   def initialize(brand)
-    @brand = brand
-  end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+  @brand=brand
+  BRANDS<<brand unless BRANDS.include?(brand)
+end
 
 end
